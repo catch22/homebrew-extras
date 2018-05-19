ce765eca236db82603a5390ad2df450ac9f2bfeb
Michael Walter <michael.walter@gmail.com>
2018-05-19 15:15:16 +0200

    barvinok: use system isl

--- barvinok.rb
+++ barvinok.rb
@@ -4,9 +4,12 @@ class Barvinok < Formula
   sha256 "b6aec6e6fd5b1574f5397cebb819c686f076a4a9d679ff023fe2999bc01054a5"
 
   depends_on "ntl"
+  depends_on "isl"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    # should use --with-isl-prefix=...?
+    system "./configure", "--prefix=#{prefix}", "--with-isl=system"
+    system "touch", "interface/all.h"
     system "make", "install"
   end
 end
